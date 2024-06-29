d0fb450175c8c9acfa7c05e58eb58f410f1c58de
Karel Vervaeke <karel.vervaeke@conundra.eu>
2024-06-29 10:32:53 +0200

    Made upload-plenaries.sh more verbose to give a feeling of progress

--- scripts/upload-plenaries.sh
+++ scripts/upload-plenaries.sh
@@ -5,7 +5,9 @@ set -e
 TARGET="http://localhost:8080"
 INPUT_PATH=../voting-data/data/output/plenary/json/plenaries.json
 
+count=0
 jq -c '.[]' "$INPUT_PATH" | while IFS= read -r obj; do
+    echo "Uploading plenary ($[count++])"
     echo "$obj" | curl --fail -H"Content-Type: application/json" --data-binary "@-" "$TARGET/plenaries"
 done
 
